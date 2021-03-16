c3c773aebde7edd4b2f6b9b1553072ffa1ad1d09
seanjib99 <78782873+seanjib99@users.noreply.github.com>
2021-03-16 16:24:46 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# every number is given on seperate lines.
+
+num1=int(input("enter the first value: "))
+num2=int(input("enter the second value:"))
+num3=int(input("enter the third value:"))
+sum=num1+num2+num3
+print(f"The sum of given three number is {sum}*")
